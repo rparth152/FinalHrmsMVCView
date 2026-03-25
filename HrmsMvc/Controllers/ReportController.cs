dea90ef3cdf220e2691f6725ffcf031500a8b582
rmeet005 <meetraval999@gmail.com>
2026-03-26 00:09:03 +0530

    .

--- HrmsMvc/Controllers/ReportController.cs
+++ HrmsMvc/Controllers/ReportController.cs
@@ -17,36 +17,58 @@ namespace HrmsMvc.Controllers
         public IActionResult Index()
         {
             var empobj = new EstatusDTO();
+
             string url = "https://localhost:7247/api/Reports/empstatus";
             HttpResponseMessage res = client.GetAsync(url).Result;
-            if (res.IsSuccessStatusCode) { 
-                var json= res.Content.ReadAsStringAsync().Result;
-                var result = JsonConvert.DeserializeObject<ApiResponse<EstatusDTO>>(json);
-                var obj = result?.Data;
-                if (obj != null) {
-                    empobj = obj;
-                }
-            }
-            
-            return View(empobj);
 
-        }
-        public IActionResult FEmp()
-        {
-            var empobj = new EmpDTO();
-            string url = "https://localhost:7247/api/Reports/fetchemp";
-            HttpResponseMessage res = client.GetAsync(url).Result;
             if (res.IsSuccessStatusCode)
             {
                 var json = res.Content.ReadAsStringAsync().Result;
-                var obj = JsonConvert.DeserializeObject<EmpDTO>(json);
+                var result = JsonConvert.DeserializeObject<ApiResponse<EstatusDTO>>(json);
+                var obj = result?.Data;
+
                 if (obj != null)
                 {
                     empobj = obj;
                 }
             }
-            TempData["EmpObj"]= empobj;
+            var empList = new List<EmpDTO>();
+            string url2 = "https://localhost:7247/api/Reports/fetchemp";
+            HttpResponseMessage res2 = client.GetAsync(url2).Result;
+
+            if (res2.IsSuccessStatusCode)
+            {
+                var json2 = res2.Content.ReadAsStringAsync().Result;
+                var result1 = JsonConvert.DeserializeObject<ApiResponse<List<EmpDTO>>>(json2);
+                var obj2 = result1?.Data;
+                if (obj2 != null)
+                {
+                    empList = obj2;
+                }
+            }
+
+            TempData["EmpObj"] = JsonConvert.SerializeObject(empList);
+
             return View(empobj);
         }
+
+        
+        //public IActionResult FEmp()
+        //{
+        //    var empobj = new EmpDTO();
+        //    string url = "https://localhost:7247/api/Reports/fetchemp";
+        //    HttpResponseMessage res = client.GetAsync(url).Result;
+        //    if (res.IsSuccessStatusCode)
+        //    {
+        //        var json = res.Content.ReadAsStringAsync().Result;
+        //        var obj = JsonConvert.DeserializeObject<EmpDTO>(json);
+        //        if (obj != null)
+        //        {
+        //            empobj = obj;
+        //        }
+        //    }
+        //    TempData["EmpObj"]= empobj;
+        //    return View(empobj);
+        //}
     }
 }
